7e23331d5adbacb212389481f47b3415f4109752
manivenkit <74611283+manivenkit@users.noreply.github.com>
2025-04-25 15:32:48 +0530

    USING SYNCFUSION 29.1.38, After changing GridHeader, @ 04/25/2025 15:32:48

--- Subscription.Model/CompanyDetails.cs
+++ Subscription.Model/CompanyDetails.cs
@@ -6,156 +6,70 @@
 // Solution:            Subscription
 // Project:             Subscription.Model
 // File Name:           CompanyDetails.cs
-// Created By:          Narendra Kumaran Kadhirvelu, Jolly Joseph Paily, DonBosco Paily, Mariappan Raja, Gowtham Selvaraj, Pankaj Sahu
-// Created On:          2-11-2024 20:37
-// Last Updated On:     3-12-2024 21:18
+// Created By:          Narendra Kumaran Kadhirvelu, Jolly Joseph Paily, DonBosco Paily, Mariappan Raja, Gowtham Selvaraj, Pankaj Sahu, Brijesh Dubey
+// Created On:          02-06-2025 16:02
+// Last Updated On:     04-24-2025 20:43
 // *****************************************/
 
 #endregion
 
 namespace Subscription.Model;
 
-[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
-[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global"), SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
 public class CompanyDetails
 {
-    public string City
+    public CompanyDetails()
     {
-        get;
-        set;
+        Clear();
     }
 
-    public string CreatedBy
-    {
-        get;
-        set;
-    }
+    public string City { get; set; }
 
-    public DateTime CreatedDate
-    {
-        get;
-        set;
-    }
+    public string CreatedBy { get; set; }
 
-    public string DUNS
-    {
-        get;
-        set;
-    }
+    public DateTime CreatedDate { get; set; }
 
-    public string EIN
-    {
-        get;
-        set;
-    }
+    public string DUNS { get; set; }
 
-    public string Extension
-    {
-        get;
-        set;
-    }
+    public string EIN { get; set; }
 
-    public string EmailAddress
-    {
-        get;
-        set;
-    }
+    public string EmailAddress { get; set; }
 
-    public string Fax
-    {
-        get;
-        set;
-    }
+    public string Extension { get; set; }
 
-    public int ID
-    {
-        get;
-        set;
-    }
+    public string Fax { get; set; }
 
-    public bool IsAdd
-    {
-        get;
-        set;
-    }
+    public int ID { get; set; }
 
-    public string LocationNotes
-    {
-        get;
-        set;
-    }
+    public bool IsAdd { get; set; }
 
-    public int NAICSCode
-    {
-        get;
-        set;
-    }
+    public string LocationNotes { get; set; }
 
-    public string Name
-    {
-        get;
-        set;
-    }
+    public string NAICS { get; set; }
 
-    public string Notes
-    {
-        get;
-        set;
-    }
+    public int NAICSCode { get; set; }
 
-    public string Phone
-    {
-        get;
-        set;
-    }
+    public string Name { get; set; }
 
-    public string State
-    {
-        get;
-        set;
-    }
+    public string Notes { get; set; }
 
-    public int StateID
-    {
-        get;
-        set;
-    }
+    public string Phone { get; set; }
 
-    public bool Status
-    {
-        get;
-        set;
-    }
+    public string State { get; set; }
 
-    public string StreetName
-    {
-        get;
-        set;
-    }
+    public int StateID { get; set; }
 
-    public string UpdatedBy
-    {
-        get;
-        set;
-    }
+    public bool Status { get; set; }
 
-    public DateTime UpdatedDate
-    {
-        get;
-        set;
-    }
+    public string StreetName { get; set; }
 
-    public string Website
-    {
-        get;
-        set;
-    }
+    public string UpdatedBy { get; set; }
 
-    public string ZipCode
-    {
-        get;
-        set;
-    }
+    public DateTime UpdatedDate { get; set; }
+
+    public string Website { get; set; }
+
+    public string ZipCode { get; set; }
 
     public void Clear()
     {
@@ -165,6 +79,7 @@ public class CompanyDetails
         DUNS = "";
         EmailAddress = "";
         ID = 0;
+        NAICS = "";
         NAICSCode = 0;
         Name = "";
         Notes = "";
