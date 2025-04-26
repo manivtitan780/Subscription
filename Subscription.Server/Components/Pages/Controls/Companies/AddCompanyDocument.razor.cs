8c12d160c65d6cafc04221ac8b3870c105050d48
manivenkit <74611283+manivenkit@users.noreply.github.com>
2025-04-26 18:53:37 +0530

    USING SYNCFUSION 29.1.39, After changing GridHeader, @ 04/26/2025 18:53:37

---
+++ Subscription.Server/Components/Pages/Controls/Companies/AddCompanyDocument.razor.cs
@@ -0,0 +1,24 @@
+#region Header
+
+// /*****************************************
+// Copyright:           Titan-Techs.
+// Location:            Newtown, PA, USA
+// Solution:            Subscription
+// Project:             Subscription.Server
+// File Name:           AddCompanyDocument.razor.cs
+// Created By:          Narendra Kumaran Kadhirvelu, Jolly Joseph Paily, DonBosco Paily, Mariappan Raja, Gowtham Selvaraj, Pankaj Sahu, Brijesh Dubey
+// Created On:          04-26-2025 18:04
+// Last Updated On:     04-26-2025 18:51
+// *****************************************/
+
+#endregion
+
+namespace Subscription.Server.Components.Pages.Controls.Companies;
+
+public partial class AddCompanyDocument : ComponentBase, IDisposable
+{
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+}
--- Subscription.Server/Components/Pages/Controls/Companies/DocumentPanel.razor.cs
+++ Subscription.Server/Components/Pages/Controls/Companies/DocumentPanel.razor.cs
@@ -19,12 +19,6 @@ public partial class DocumentPanel
 {
     private int _selectedID;
 
-    /// <summary>
-    ///     Gets or sets the event callback that triggers when an education detail is to be deleted.
-    /// </summary>
-    /// <value>
-    ///     The event callback for deleting an education detail.
-    /// </value>
     [Parameter]
     public EventCallback<int> DeleteCompanyDocument
     {
@@ -39,12 +33,6 @@ public partial class DocumentPanel
         set;
     }
 
-    /// <summary>
-    ///     Gets or sets the event callback that triggers when an education detail is to be edited.
-    /// </summary>
-    /// <value>
-    ///     The event callback for editing an education detail.
-    /// </value>
     [Parameter]
     public EventCallback<int> EditCompanyDocument
     {
@@ -72,52 +60,23 @@ public partial class DocumentPanel
         set;
     } = 40;
 
-    /// <summary>
-    ///     Gets the selected row in the grid. This property is of type <see cref="CompanyDocuments" />.
-    /// </summary>
-    /// <value>
-    ///     The selected row in the grid, represented as a CandidateEducation object.
-    /// </value>
-    /// <remarks>
-    ///     This property is set internally when a row is selected in the grid. It is used to hold the education details of the
-    ///     selected candidate.
-    /// </remarks>
     private CompanyDocuments SelectedRow
     {
         get;
         set;
     }
 
-    /// <summary>
-    ///     Asynchronously deletes the education detail of a candidate.
-    /// </summary>
-    /// <param name="id">The ID of the education detail to be deleted.</param>
-    /// <returns>A Task representing the asynchronous operation.</returns>
-    /// <remarks>
-    ///     This method sets the selected ID to the provided ID, gets the index of the row in the grid corresponding to the ID,
-    ///     selects the row in the grid, and shows a confirmation dialog.
-    /// </remarks>
     private async Task DeleteCompanyDocumentMethod(int id)
     {
         _selectedID = id;
         int _index = await Grid.GetRowIndexByPrimaryKeyAsync(id);
         await Grid.SelectRowAsync(_index);
-        //await DialogConfirm.ShowDialog();
         if (await DialogService.ConfirmAsync(null, "Delete Company Document", General.DialogOptions("Are you sure you want to <strong>delete</strong> this <i>Company Document</i>?")))
         {
             await DeleteCompanyDocument.InvokeAsync(_selectedID);
         }
     }
 
-    /// <summary>
-    ///     Asynchronously opens the dialog for editing the education details of a candidate.
-    /// </summary>
-    /// <param name="id">The ID of the education detail to be edited.</param>
-    /// <returns>A Task representing the asynchronous operation.</returns>
-    /// <remarks>
-    ///     This method sets the selected ID to the provided ID, gets the index of the row in the grid corresponding to the
-    ///     provided ID, selects the row in the grid, and invokes the EditEducation event callback.
-    /// </remarks>
     private async Task EditCompanyDocumentDialog(int id)
     {
         _selectedID = id;
@@ -126,14 +85,6 @@ public partial class DocumentPanel
         await EditCompanyDocument.InvokeAsync(id);
     }
 
-    /// <summary>
-    ///     Handles the row selection event in the Documents details grid.
-    /// </summary>
-    /// <param name="document">The event arguments containing the selected row data of type <see cref="CompanyDocuments" />.</param>
-    /// <remarks>
-    ///     This method is triggered when a row is selected in the education details grid.
-    ///     It sets the SelectedRow property to the data of the selected row.
-    /// </remarks>
     private void RowSelected(RowSelectEventArgs<CompanyDocuments> document)
     {
         if (document != null)
